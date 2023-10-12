49142ad694816babffb7343c23d4dbd09fef0da4
multilucas <lucaslopes198@gmail.com>
2023-10-12 11:46:16 -0300

    init of project of laravel

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index() {
+        return view('produto.main', [ 'produtos' => Produto::all()]);
+    }
+}
