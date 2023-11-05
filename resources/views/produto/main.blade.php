5f9cd4121c04dcd290dcf326c3df1d046acb1ca8
multilucas <lucaslopes198@gmail.com>
2023-11-04 22:12:27 -0300

    modal of products is working, show name, price, description and image products

--- resources/views/produto/main.blade.php
+++ resources/views/produto/main.blade.php
@@ -159,6 +159,7 @@
         </button>
     </div>
     <!-- FIM Carrousel -->
+
     <!-- CARDS -->
     <div class="bg-white">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -174,10 +175,84 @@
                         <!-- Modal -->
                         <div x-show="showModal" x-on:click.outside="showModal = false" class="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50">
                             <div class="bg-white p-4 rounded-lg shadow-xl">
-                                <!-- Conteúdo do modal -->
-                                <h1 class="text-xl font-semibold mb-2">{{ $produto->PRODUTO_NOME }}</h1>
-                                <p>Descrição do produto.</p>
-                                <button x-on:click="showModal = false" class="mt-4 bg-blue-700 text-white py-2 px-4 rounded-md hover:bg-blue-800">Fechar</button>
+                                <!-- Conteudo do modal -->
+
+                                <div class="relative z-10" role="dialog" aria-modal="true">
+                                    <div class="fixed inset-0 hidden bg-gray-500 bg-opacity-75 transition-opacity md:block"></div>
+
+                                    <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                        <div class="flex min-h-full items-stretch justify-center text-center md:items-center md:px-2 lg:px-4">
+                                            <div class="flex w-full transform text-left text-base transition md:my-8 md:max-w-2xl md:px-4 lg:max-w-4xl">
+                                                <div class="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                                                    <button type="button" class="absolute right-4 top-4 text-gray-400 hover:text-gray-500 sm:right-6 sm:top-8 md:right-6 md:top-6 lg:right-8 lg:top-8">
+                                                        <span class="sr-only">Close</span>
+                                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                                        </svg>
+                                                    </button>
+
+                                                    <div class="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+                                                        <div class="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
+                                                            <img class="p-8 rounded-t-lg w-full h-60 cursor-pointer" src="{{ $produto->imagem->first()->IMAGEM_URL ?? '' }}" alt="product image">
+                                                        </div>
+                                                        <div class="sm:col-span-8 lg:col-span-7">
+                                                            <h2 class="text-2xl font-bold text-gray-900 sm:pr-12">{{ $produto->PRODUTO_NOME}}</h2>
+
+                                                            <section aria-labelledby="information-heading" class="mt-2">
+                                                                <h3 id="information-heading" class="sr-only">Product information</h3>
+
+                                                                <p class="text-2xl text-gray-900">{{$produto->PRODUTO_PRECO}}</p>
+
+                                                                <!-- Reviews -->
+                                                                <div class="mt-6">
+                                                                    <h4 class="sr-only">Reviews</h4>
+                                                                    <div class="flex items-center">
+                                                                        <div class="flex items-center">
+                                                                            <!-- Active: "text-gray-900", Default: "text-gray-200" -->
+                                                                            <svg class="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                                                                            </svg>
+                                                                            <svg class="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                                                                            </svg>
+                                                                            <svg class="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                                                                            </svg>
+                                                                            <svg class="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                                                                            </svg>
+                                                                            <svg class="text-gray-200 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                                                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+                                                                            </svg>
+                                                                        </div>
+                                                                        <p class="sr-only">3.9 out of 5 stars</p>
+                                                                        <a href="#" class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reviews</a>
+                                                                    </div>
+                                                                </div>
+                                                            </section>
+
+                                                            <section aria-labelledby="options-heading" class="mt-10">
+                                                                <h3 id="options-heading" class="sr-only">Product options</h3>
+
+                                                                <form>
+                                                                    <!-- Colors -->
+
+
+                                                                    <!-- Sizes -->
+                                                                    <div class="mt-10">
+                                                                        <p class="text-xl font-semibold tracking-tight text-gray-900 "">{{ $produto->PRODUTO_DESC }}</p>
+                                                                    </div>
+
+                                                                    <button type="submit" class="mt-6 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Add to bag</button>
+                                                                </form>
+                                                            </section>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
