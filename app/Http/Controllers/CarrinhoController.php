433e63adbcbcfd8be0be2cc1e4541df50e3b93a9
multilucas <lucaslopes198@gmail.com>
2023-11-15 20:22:50 -0300

    fixing the logic of add a product which exists :q

--- app/Http/Controllers/CarrinhoController.php
+++ app/Http/Controllers/CarrinhoController.php
@@ -13,23 +13,25 @@ class CarrinhoController extends Controller
         if (auth()->check()) {
             $user = auth()->user();
             $carrinhoItem = Carrinho::where('USUARIO_ID', $user->USUARIO_ID)->where('PRODUTO_ID', $produtoId)->first();
+
             if ($carrinhoItem) {
-                $carrinhoItem->ITEM_QTD += 1;
+                // Product already exists in the cart, you can choose to update the quantity or ignore
                 $carrinhoItem->save();
+                return redirect()->route('carrinho.index')->with('success', 'Produto adicionado ao carrinho.');
             } else {
+                // Product doesn't exist in the cart, add it
                 Carrinho::create([
                     'USUARIO_ID' => $user->USUARIO_ID,
                     'PRODUTO_ID' => $produtoId,
                     'ITEM_QTD' => 1
                 ]);
+                return redirect()->route('carrinho.index')->with('success', 'Produto adicionado ao carrinho.');
             }
-            return redirect()->route('carrinho.index')->with('success', 'Produto adicionado ao carrinho.');
         } else {
             // Se o usuário não estiver autenticado, redirecione para a página de login
             return redirect()->route('login')->with('error', 'Você precisa fazer login para adicionar produtos ao carrinho.');
         }
     }
-
     public function index(){
         // Recupere os itens do carrinho do usuário autenticado
         $categorias = Categoria::all();
